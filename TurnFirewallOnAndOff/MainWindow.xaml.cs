c884c4d839deda29307424750b7743f91836fd4b
Matan Fachima <mf250333@ncr.com>
2022-02-07 13:13:55 +0200

    Add project files.

---
+++ TurnFirewallOnAndOff/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace TurnFirewallOnAndOff
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _firewallStatus;
+
+        public string FirewallStatus
+        {
+            get => _firewallStatus;
+            set
+            {
+                _firewallStatus = value;
+                OnPropertyChanged(nameof(FirewallStatus));
+            }
+        }
+        public MainWindow()
+        {
+            DataContext = this;
+            FirewallStatus = "";
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OnButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (FirewallStatus == "On") return;
+                string batchCommand = "netsh advfirewall set allprofiles state on";
+                string command = $"/c {batchCommand}";
+                Process.Start("CMD.exe", command);
+                FirewallStatus = "On";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OffButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (FirewallStatus == "Off") return;
+                string batchCommand = "netsh advfirewall set allprofiles state off";
+                string command = $"/c {batchCommand}";
+                Process.Start("CMD.exe", command);
+                FirewallStatus = "Off";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
